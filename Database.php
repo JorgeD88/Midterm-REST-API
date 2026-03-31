b62f66663237d336393a9f1b967445d9ad230f9c
JorgeD88 <98435442+JorgeD88@users.noreply.github.com>
2026-03-31 16:07:54 -0500

    Refactor Database class to use environment variables

--- Database.php
+++ Database.php
@@ -1,18 +1,30 @@
 <?php
 
 class Database {
-    private $host = 'db.nmbjhwzhuporyoqvxiih.supabase.co';
-    private $port = '5432';
-    private $db_name = 'postgres';
-    private $username = 'postgres';
-    private $password = '@K1ll3r176Jd2000';
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST') ?: 'db.nmbjhwzhuporyoqvxiih.supabase.co';
+        $this->port = getenv('DB_PORT') ?: '5432';
+        $this->db_name = getenv('DB_NAME') ?: 'postgres';
+        $this->username = getenv('DB_USER') ?: 'postgres';
+        $this->password = getenv('DB_PASSWORD') ?: '@K1ll3r176Jd2000';
+    }
 
     public function connect() {
         $this->conn = null;
 
         try {
-            $dsn = 'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name;
+            $dsn = 'pgsql:host=' . $this->host .
+                   ';port=' . $this->port .
+                   ';dbname=' . $this->db_name .
+                   ';sslmode=require';
+
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
